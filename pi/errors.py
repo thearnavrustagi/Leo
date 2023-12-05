2af9119a666f681e468bd780dc0d5010095d5ff1
arnav rustagi <arnrustagi@gmail.com>
2023-12-05 23:19:07 +0530

    updated pi code

--- pi/errors.py
+++ pi/errors.py
@@ -2,9 +2,12 @@ class NoPathError(Exception):
     def __init__(self):
         super().__init__(f"No possible path found")
 
-
 class IllegalStartPositionError(Exception):
     def __init__(self, position):
         super().__init__(
             f"the position {position} is already occupied, kindly pick a better position"
         )
+
+class QuitError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
